5531cca641f4e4f2dfdf9e967dc7f6ec7915401c
Adam Blaq <adamblaq1@gmail.com>
2026-03-18 23:22:23 +0000

    Initial Commit

---
+++ StremioClient/StremioClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  StremioClientApp.swift
+//  StremioClient
+//
+//  Created by Adam Blaq on 18/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct StremioClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
